1895f85d3c496e201115f1b476d690e9863eee74
jan-preisig <jan_preisig@outlook.com>
2021-04-29 12:06:21 +0200

    readme & code quality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,37 +32,46 @@ export class AppComponent implements OnDestroy {
     const files = $event.target.files;
     this.ngxCsvParser.parse(files[0], {header: this.header, delimiter: ';'})
       .pipe().subscribe((result: Array<any>) => {
-      console.log('Result', result);
       this.spellcards = result;
       this.spellcards.forEach((card, index) => {
         const countLinebreaks = (card.beschreibung.match('<br>') || []).length;
         const pageNr = card.page ? card.page : 1;
         if (card.beschreibung.includes('-p2-')) {
-          const splitted = card.beschreibung.split('-p2-');
-          card.beschreibung = splitted[0];
-          this.spellcards.splice(index + 1, 0, {
-            titel: (card.mainTitle ? card.mainTitle : card.titel) + ' ' + (pageNr + 1),
-            mainTitle: card.titel,
-            beschreibung: splitted[1],
-            page: (pageNr + 1)
-          });
+          this.manualLinebrak(card, index, pageNr);
         }
         let maxlength = 1200 - (countLinebreaks * 100);
         maxlength = card.beschreibung.indexOf(' ', maxlength);
         const descPage2 = '...' + card.beschreibung.substr(maxlength, card.beschreibung.length - maxlength);
         if (card.beschreibung.length > maxlength && descPage2.length > 10) {
-          this.spellcards.splice(index + 1, 0, {
-            titel: (card.mainTitle ? card.mainTitle : card.titel) + ' ' + (pageNr + 1),
-            mainTitle: card.titel,
-            beschreibung: descPage2,
-            page: (pageNr + 1)
-          });
-          card.beschreibung = card.beschreibung.substr(0, maxlength) + '...';
+          this.autoLinebreak(index, card, pageNr, descPage2, maxlength);
         }
       });
     }, (error: NgxCSVParserError) => {
       console.log('Error', error);
     });
+  }
+
+  private autoLinebreak(index: number, card, pageNr, descPage2: string, maxlength: number): void {
+    this.spellcards.splice(index + 1, 0, {
+      titel: (card.mainTitle ? card.mainTitle : card.titel) + ' ' + (pageNr + 1),
+      mainTitle: card.titel,
+      beschreibung: descPage2,
+      page: (pageNr + 1)
+    });
+    card.beschreibung = card.beschreibung.substr(0, maxlength) + '...';
+  }
+
+  private manualLinebrak(card, index: number, pageNr): void {
+    const splitted = card.beschreibung.split('-p2-');
+    card.beschreibung = splitted[0];
+    for (let i = 1; i < splitted.length; i++) {
+      this.spellcards.splice(index + i, 0, {
+        titel: (card.mainTitle ? card.mainTitle : card.titel) + ' ' + (pageNr + i),
+        mainTitle: card.titel,
+        beschreibung: splitted[i],
+        page: (pageNr + i)
+      });
+    }
   }
 
   ngOnDestroy(): void {
